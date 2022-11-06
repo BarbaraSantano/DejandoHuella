4c7e039d4e918819768d0102f6bed33472f1b8eb
chantell <chantellgebauer@gmail.com>
2022-11-06 19:13:02 +0100

    navbar

--- front-dejando-huella/src/App.js
+++ front-dejando-huella/src/App.js
@@ -1,10 +1,13 @@
 
 import './App.css';
 import Animal from './componentes/Animal';
+import Navbar from './componentes/Navbar.jsx';
+
+
 function App() {
   return (
     <div className="App">
-     
+     <Navbar />
      <Animal/>
      
 
---
+++ front-dejando-huella/src/componentes/Navbar.jsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import logo from '../media/logo-horizontal.png';
+import '../estilos/Navbar.css';
+
+const Navbar = () => {
+
+    //setting mobile nav
+    const [click, setClick] = useState(false)
+    const handleClick = () => setClick(!click)
+
+    //change nav color when scrolling
+    const [color, setColor] = useState(false)
+    const changeColor = () => {
+        if (window.scrollY >= 36) {
+            setColor(true)
+        } else {
+            setColor(false)
+        }
+    }
+
+    window.addEventListener('scroll', changeColor)
+
+    // close menu onClick
+    const closeMenu = () => setClick(false)
+
+    return (
+ 
+        <div className={color ? 'header header-bg' : 'header'}>
+            <nav className='navbar'>
+                <a href='/' className='logo'>
+                    <img src={logo} alt='logo' />
+                </a>
+                <div className='hamburger' onClick={handleClick}>
+                    {click ? (<FaTimes size={30} style={{ color: '#ffffff' }} />)
+                        : (<FaBars size={30} style={{ color: '#ffffff' }} />)}
+
+                </div>
+                <ul className={click ? "nav-menu active" : "nav-menu"}>
+                <li className='nav-item'>
+                      <a href='Inicio' onClick={closeMenu}>INICIO</a>
+                    </li>
+                    <li className='nav-item'>
+                      <a href='adoptaPerro' onClick={closeMenu}>PERROS</a>
+                    </li>
+
+                    <li className='nav-item'>
+                      <a href='adoptaGatos' onClick={closeMenu}>GATOS</a>
+                    </li>
+                   
+                    <li className='nav-item'>
+                        <a href='#acercaDe' onClick={closeMenu}>NOSOTROS</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#contactanos' onClick={closeMenu}>CONTACTO</a>
+                    </li>
+                    <li className='nav-item'>
+                        <a href='#colabora' onClick={closeMenu}>COLABORA</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+
+    )
+}
+
+export default Navbar;
