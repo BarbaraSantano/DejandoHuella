c150b0a2e3a4c9952eb74e383d763ee3d5e6abbe
Valentina Marta <mvlory30@gmail.com>
2022-11-22 12:03:28 +0100

    email contacto

--- front-dejando-huella/src/componentes/Formulario.jsx
+++ front-dejando-huella/src/componentes/Formulario.jsx
@@ -3,28 +3,36 @@ import '../estilos/formulario.css';
 import perro1 from '../media/perroformu1.jpg';
 import perro2 from '../media/gatoyperroformu.jpg';
 import perro3 from '../media/gatoformu1.jpg';
+import emailjs from 'emailjs-com';
 
 export const Formulario = () => {
+    function enviarEmail(e){
+        e.preventDefault();
+        emailjs.sendForm('service_75ei1kv','template_0tyjflx',e.target,'pfYc5nnw5aMg1owHl').then(res=> {
+          alert("Se ha enviado correctamente.");
+          console.log(res);  
+        })
+    }
   return (
-    <div id="containergral">
-    <div id="containerfotos">
+    <div className="containergral">
+    <div className="containerfotos">
         <img src={perro1} alt="" />
         <img src={perro3} alt="" />
         <img src={perro2} alt="" />
     </div>
-    <h1 id="titulocentrado">Contacta con nosotrxs: </h1>
-        <form>
-            <div id="formunombre">
+    <h1 className="titulocentrado">Contacta con nosotrxs: </h1>
+        <form onSubmit={enviarEmail}>
+            <div className="formunombre">
                 <input type="text" placeholder="Nombre" id="nombre"/>
             </div>
-            <div id="formuemail">
-                <input type="text" placeholder="Email" id="email"/>
+            <div className="formuemail">
+                <input type="email" placeholder="Email" id="email"/>
             </div>
-            <div id="formumensaje">
-                <input type="textarea" placeholder="Mensaje" id="mensaje"/>
+            <div className="formumensaje">
+                <input type="text" placeholder="Mensaje" id="mensaje"/>
             </div>
             <div>
-                <button type="submit" id="botonformu">Enviar</button>
+                <button type="submit" className="botonformu">Enviar</button>
             </div>
         </form>
     </div>
